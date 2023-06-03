8f95b50f515c194c148c859374feda2ed016241c
SeongHun <tjdgns5506@gmail.com>
2023-06-03 11:06:37 +0900

    Finished Gatsby Tutorial Part 4

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 
 // Step 2: Define your component
@@ -13,7 +14,6 @@ const AboutPage = () => {
 }
 
 // Step 3: Export your component
-export const Head = () => <title>About Me</title>
-
+export const Head = () => <Seo title='About Me'/>
 
 export default AboutPage
